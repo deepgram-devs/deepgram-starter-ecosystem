31da272dce339dbb191616d6e49b134e5e4f75da
Naomi Carrigan <commits@nhcarrigan.com>
2025-08-04 10:13:32 -0700

    feat: it is live on fly

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // TODO: turn this off when ready for production
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
 };
 
 export default nextConfig;
