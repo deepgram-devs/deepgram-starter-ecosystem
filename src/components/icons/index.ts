b10e4b706d757ed0a24a47c24cc40dc181c83a26
John Vajda <john.vajda@deepgram.com>
2025-08-14 14:38:17 -0600

    feat: adds language icons

---
+++ src/components/icons/index.ts
@@ -0,0 +1,11 @@
+// Language icons export
+export { JavaScriptIcon } from './JavaScriptIcon';
+export { PythonIcon } from './PythonIcon';
+export { GoIcon } from './GoIcon';
+export { JavaIcon } from './JavaIcon';
+export { CSharpIcon } from './CSharpIcon';
+export { NodeJsIcon } from './NodeJsIcon';
+export { NextJsIcon } from './NextJsIcon';
+export { RubyIcon } from './RubyIcon';
+export { PhpIcon } from './PhpIcon';
+export { DotNetIcon } from './DotNetIcon';
